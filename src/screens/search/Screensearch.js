205b08893a84bf42aa1dfbdd6ccf200f80d099be
leoromero97 <leonardogerbacio@gmail.com>
2020-10-30 10:17:17 -0300

    Se elemino el array de peliculas que simulaba la api

--- src/screens/search/Screensearch.js
+++ src/screens/search/Screensearch.js
@@ -3,13 +3,12 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import Search from "../../components/search/search";
 import ListMovies from "../../components/containerMovieCard/ListMovies";
-import ARRAYMOVIES from "../../services/arrayMovie";
 
 export default function SearchMovie({ navigation }) {
   return (
     <View style={styles.container}>
       <Search placeholder="Enter a search from" />
-      <ListMovies movies={ARRAYMOVIES} navigation={navigation} />
+      <ListMovies navigation={navigation} />
     </View>
   );
 }
