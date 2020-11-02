e9932a97b4800d11190aa4873d5e7b383ba6432d
leoromero97 <leonardogerbacio@gmail.com>
2020-11-02 17:53:16 -0300

    Se corrigieron coments del pull request

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import ScreenMovie from "./src/screens/movie/ScreenMovie";
 const Stack = createStackNavigator();
 
 export default function App() {
+  console.log(process.env);
   return (
     <NavigationContainer>
       <Stack.Navigator
