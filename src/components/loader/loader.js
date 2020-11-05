57d36815bc481302909cc8ff2951ca867ad912e5
leoromero97 <leonardogerbacio@gmail.com>
2020-11-05 15:05:28 -0300

    se creo el componente loader que se muestra cuando carga al buscar la info de la pelicula selecionada.

---
+++ src/components/loader/loader.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, Animated } from "react-native";
+
+export default function Loader() {
+  return (
+    <Animated.View style={styles.continerAnimation}>
+      <Animated.Text style={styles.text}>Cargando</Animated.Text>
+      <Animated.View style={styles.circles}></Animated.View>
+      <Animated.View style={styles.circles}></Animated.View>
+      <Animated.View style={styles.circles}></Animated.View>
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  continerAnimation: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#303030",
+  },
+
+  text: {
+    fontSize: 20,
+    color: "#f2f2f2",
+    margin: 14,
+  },
+
+  circles: {
+    width: 10,
+    height: 10,
+    backgroundColor: "#f2f2f2",
+    borderRadius: 50,
+    margin: 10,
+  },
+});
